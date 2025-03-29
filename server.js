5ee646711d8fecf998f317cb81fdb854ae549e17
Harvey Lopez <up2118183@myport.ac.uk>
2025-03-29 19:40:07 +0000

    Added: First implementation of runner/ volunteer account splitting at login, need to debug the check against the current database.

--- server.js
+++ server.js
@@ -9,12 +9,17 @@ const __dirname = path.dirname(__filename);
 // creating instance of express server
 const app = express();
 
-// example db
-const users = [
-  {id: 100, username: "admin", password: "adminPassword"},
+// example db for runners and users
+const runners = [
+  {id: 100, username: "adminR", password: "adminPassword"},
   {id: 101, username: "firstUser", password: "userPassword"},
-  {id: 102, username: "firstVolunteer", password: "volunteerPassword"}
+  {id: 102, username: "secondUser", password: "userPassword2"}
 ];
+const volunteers = [
+  {id: 500, username: "adminV", password: "adminPasswordV"},
+  {id: 501, username: "firstVolunteer", password: "volunteerPassword"},
+  {id: 502, username: "secondVolunteer", password: "volunteerPassword2"}
+]
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -29,27 +34,47 @@ app.use((req, res, next) => {
 app.use(express.static(path.resolve(__dirname, './webpages')));
 app.use(express.static(path.resolve(__dirname,'./src')));
 
-app.get('/', (req, res) => {
+function getIndex(req, res) {
   res.sendFile(path.resolve(__dirname, './webpages/index.html'));
-})
-
-app.post('/login', (req, res) => {
-  const { username, password } = req.body;
+}
+//needs debugging
+function checkUser(database, username, password) {
+  for (let user of database) {
+    if (user.username === username && user.password === password) {
+      console.log("User Found!!");
+      return true;
+    } else {
+      console.log("404: User Not Found");
+      return false;
+    }
+  }
+}
+function postLogin(req, res) {
+  const { accountType, username, password } = req.body;
+  
+  // debugging log
+  console.log("Form Data Received: ", req.body);
+  
+  if (!['runners', 'volunteers'].includes(accountType)) {
+    
+    res.send("Invalid accountType", req.body);
+    console.log("Invalid accountType", req.body);
+  }
+  console.log("Account Type:" , accountType);
+  const db = accountType === 'runners' ? runners : volunteers;
 
   // Check if user exists in database
-  const user = users.find(u => u.username === username && u.password === password); 
-
-  if (user) {
+  if (checkUser(db, username, password)) {
     console.log(`Successful login, welcome ${user.username}!`);
     res.redirect('/home.html');
   } else {
-    res.status(401).send("Invalid Username");
+    res.status(401).send("Invalid Username or Password.");
+    console.log("Invalid username and/ or password");
   }
-  console.log(req.body);
-  console.log(res);
-  console.log(user);
-  console.log(users);
-});
+}
+
+app.get('/', getIndex);
+app.post('/login', postLogin)
 
 app.use((req, res) => {
   res.status(404).send("Error Code 404: Page not found");
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-/*
-const isLoggedIn = true;
-const loginSection = document.querySelector('#login-section');
+const accountType = document.querySelector('#account-type');
+const runnerCheckbox = document.querySelector('#runner-chk');
+const volunteerCheckbox = document.querySelector('#volunteer-chk');
 
-if (!isLoggedIn) {
-  loginSection.style.display = 'block';
-} else {
-  loginSection.style.display = 'none';
-}
-*/
+runnerCheckbox.addEventListener('change', () => {
+  if (runnerCheckbox.checked) {
+    accountType.value = 'volunteers';
+    volunteerCheckbox.checked = false;
+  }
+  console.log("runner checked");
+});
+
+volunteerCheckbox.addEventListener('change', () => {
+  if (volunteerCheckbox.checked) {
+    accountType.value = 'runners';
+    runnerCheckbox.checked = false;
+  }
+  console.log("volunteer checked");
+})
