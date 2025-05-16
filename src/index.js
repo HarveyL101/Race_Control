2a6a4bfdf84092e7e32d91661aa87f558cf87b38
Harvey <UP2118183@myport.ac.uk>
2025-05-16 01:42:44 +0100

    Added: implemented handlers for swapping between Login and Register.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import { Login } from './components/util.js';
+import { Register } from './components/login/Register.js';
 
 customElements.define('login-panel', Login);
+customElements.define('register-panel', Register);
+
+const swapper = {
+    text: document.querySelector('#swapper-text'),
+    link: document.querySelector('#swapper-link')
+};
+
+function handleTemplate() {
+    const login = document.querySelector('#login-panel');
+    const register = document.querySelector('#register-panel');
+    
+    
+    const isLoginVisible = login.style.display !== "none";
+
+    login.style.display = isLoginVisible ? "none" : "block";
+    register.style.display = isLoginVisible ? "block" : "none";
+
+    swapper.text.textContent = isLoginVisible ? "Already have an account?" : "Don't have an account?";
+    swapper.link.textContent = isLoginVisible ? "Login Here" : "Register Here";
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    handleTemplate();
+
+    swapper.link.addEventListener('click', handleTemplate);
+});
